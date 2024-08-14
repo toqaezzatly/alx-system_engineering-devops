b790d28215ed3f75c6b2151617c4049f7c2b2fa4
root <toqaezzatly@gmail.com>
2024-08-14 04:03:12 -0700

    puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,6 @@
-# 0-strace_is_your_friend.pp
-# This Puppet manifest installs the missing PHP module required by WordPress
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-package { 'php5-mysql':
-  ensure => installed,
-}
-
-service { 'apache2':
-  ensure => running,
-  enable => true,
-  subscribe => Package['php5-mysql'],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
